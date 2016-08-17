133e0e4455cf5cd41b2e7a51ca5a339318e14728
agate <agate.hao@gmail.com>
2016-08-17 14:59:14 +0800

    init

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+[[ $VCL_CONFIG == http* ]] && \
+  curl -s $VCL > /etc/varnish/custom.vcl && \
+  VCL_CONFIG="/etc/custom.vcl"
+
+[[ $START_SCRIPT == http* ]] && \
+  curl -s $START_SCRIPT > /start_script && \
+  START_SCRIPT="/start_script" && \
+  chmod +x $START_SCRIPT
+if [ -n "$START_SCRIPT" ]; then
+  $START_SCRIPT
+fi
+
+set -e
+
+exec bash -c \
+  "exec varnishd -F -u varnish \
+  -f $VCL_CONFIG \
+  -s malloc,$CACHE_SIZE \
+  $VARNISHD_PARAMS"
